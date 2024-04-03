599ba0f53efd2abca476d9886cc705091bc0fa10
Savinoff94 <andreisavinovwork@gmail.com>
2024-04-04 01:05:48 +0300

    removed anchor tag from Friend Block

--- src/pages/Friends/components/FriendBlockNew/FriendBlockNew.tsx
+++ src/pages/Friends/components/FriendBlockNew/FriendBlockNew.tsx
@@ -11,7 +11,7 @@ export default function FriendBlock({simpleStats, login, id, buttonsInfosList, i
     return (
 
         <li data-isalone={isAlone} className='flex justify-between pb-1 pt-1 data-[isalone=false]:border-b data-[isalone=false]:border-purple-200'  key={id} id={id}>
-            <a className='flex justify-evenly items-center w-2/3' href={'http://localhost:5000/api/charts/:'+id}>
+            <div className='flex justify-evenly items-center w-2/3'>
                 <div className='flex-col'>
                     <BasketballIcon/>
                     {/* <img alt='profile' src={friendBlockProps['imageSrc']}/> */}
@@ -20,7 +20,7 @@ export default function FriendBlock({simpleStats, login, id, buttonsInfosList, i
                 <div className='flex font-sofia'>1pt: {simpleStats['freethrows']}%</div>
                 <div className='flex font-sofia'>2pt: {simpleStats['twoPointers']}%</div>
                 <div className='flex font-sofia'>3pt: {simpleStats['threePointers']}%</div>
-            </a>
+            </div>
             <div className='flex gap-1 flex-col sm:flex-row items-center'>
             {
                 buttonsInfosList.map((buttonInfo: FriendActionButtonInfo): JSX.Element => {

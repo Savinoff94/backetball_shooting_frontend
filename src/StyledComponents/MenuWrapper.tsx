6f76228737d851821b07a83d82ffffca2fa696c7
Savinoff94 <andreisavinovwork@gmail.com>
2024-05-26 12:35:00 +0300

    menu wrapper fix

--- src/StyledComponents/MenuWrapper.tsx
+++ src/StyledComponents/MenuWrapper.tsx
@@ -7,7 +7,8 @@ type MenuWrapperProps = {
 function MenuWrapper({children} : MenuWrapperProps) {
 
     return (
-        <div className="flex gap-3 flex-col self-center w-full h-96 sm:w-96 justify-evenly opacity-90 items-center rounded-lg shadow-lg ml-2 mr-2 sm:ml-0 sm:mr-0">
+        // <div className="flex gap-3 flex-col self-center w-full h-96 sm:w-96 justify-evenly opacity-90 items-center rounded-lg shadow-lg ml-2 mr-2 sm:ml-0 sm:mr-0">
+        <div className="flex gap-3 flex-col self-center w-full h-96 sm:w-96 justify-evenly opacity-90 items-center ml-2 mr-2 sm:ml-0 sm:mr-0">
             {children}
         </div>
     )

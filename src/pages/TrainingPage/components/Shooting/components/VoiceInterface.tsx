6e3bc095de00a9b116db0d638b8bef3b4d31e96e
Savinoff94 <andreisavinovwork@gmail.com>
2024-06-13 20:20:20 +0300

    WIP: voice input styling

--- src/pages/TrainingPage/components/Shooting/components/VoiceInterface.tsx
+++ src/pages/TrainingPage/components/Shooting/components/VoiceInterface.tsx
@@ -34,61 +34,54 @@ export default function VoiceInterface() {
     }, [recognizerRef])
     
 
-    function qqq() {
-
-        return async () => {
-
-            const recognizer = recognizerRef.current;
-            
-            if(!recognizer) {
-                return
-            }
-
-            if(isListening) {
-                recognizer.stopListening()
-                setIsListening(false)
-                return
-            }
     
-            setIsListening(true)
-            await recognizer.ensureModelLoaded();
-            recognizer.listen(async (result) => {
 
-                const scores = Array.from(result.scores as Float32Array).map((s, i) => ({score: s, word: recognizer.wordLabels()[i]}));
-                // Find the most probable word.
-                scores.sort((s1, s2) => s2.score - s1.score);
+    const onClick = async () => {
 
-                const myWord = scores[0].word;
-
-                if(myWord === 'yes') {
-
-                    trainingBoardStore.shotMade()
-                }
-
-                if((myWord === 'no') || (myWord === 'go')) {
-
-                    trainingBoardStore.shotMissed()
-                }
-            }, {
-                includeSpectrogram: true,
-                probabilityThreshold: 0.75,
-                overlapFactor: 0
-            });
+        const recognizer = recognizerRef.current;
+        
+        if(!recognizer) {
+            return
         }
+
+        if(isListening) {
+            recognizer.stopListening()
+            setIsListening(false)
+            return
+        }
+
+        setIsListening(true)
+        await recognizer.ensureModelLoaded();
+        recognizer.listen(async (result) => {
+
+            const scores = Array.from(result.scores as Float32Array).map((s, i) => ({score: s, word: recognizer.wordLabels()[i]}));
+            // Find the most probable word.
+            scores.sort((s1, s2) => s2.score - s1.score);
+
+            const myWord = scores[0].word;
+
+            if(myWord === 'yes') {
+
+                trainingBoardStore.shotMade()
+            }
+
+            if((myWord === 'no') || (myWord === 'go')) {
+
+                trainingBoardStore.shotMissed()
+            }
+        }, {
+            includeSpectrogram: true,
+            probabilityThreshold: 0.75,
+            overlapFactor: 0
+        });
+        
     }
-
-
-    const onClick = qqq()
-
     
-    
-    
-
 
     return (
-        <button className={`${isListening ? 'bg-main' : 'bg-main'} flex rounded-full items-center justify-center h-[100px] w-[100px]`} onClick={() => {onClick()}}>
+        <button className={`${isListening ? 'bg-gradient-to-r from-cyan-500 to-blue-500 opacity-70' : 'bg-main'} transition duration-500 flex rounded-full items-center justify-center h-[100px] w-[100px]`} onClick={() => {onClick()}}>
             <MicrophoneIcon/>
-            {isListening && <span className="animate-ping absolute inline-flex h-[100px] w-[100px] rounded-full bg-sky-400 opacity-75"></span>}
+            {isListening && <span className="animate-ping  absolute inline-flex h-[100px] w-[100px] rounded-full bg-sky-400 opacity-75"></span>}
         </button>
     )
 }
